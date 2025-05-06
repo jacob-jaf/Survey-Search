2dd2cfbe53603d95e24f2f70e640bac425e9e879
jacob-jaf <141459775+jacob-jaf@users.noreply.github.com>
2025-05-06 14:41:22 -0700

    Update Rope
    Checking to see how it detects similarity to Afghanistan question example.

--- RoPE_Surveys.py
+++ RoPE_Surveys.py
@@ -4,6 +4,7 @@ import torch
 from sentence_transformers import SentenceTransformer, util
 import pandas as pd
 from pathlib import Path
+import numpy as np
 
 #os.path.dirname(os.path.abspath(__file__))
 #Path(__name__).resolve()
@@ -21,13 +22,25 @@ ces_questions = pd.read_csv(
 )
 ces_questions['question_only'] = ces_questions['Text'].str.split('<').str[0]
 
-#get the entire column:
-ces_questions['question_only']
 
 ces_model = SentenceTransformer('paraphrase-mpnet-base-v2')
-ces_embeddings = model.encode(ces_questions['question_only'])
-ces_similarities = model.similarity(ces_embeddings, ces_embeddings)
-ces_similarities.fill_diagonal_(0)
+ces_embeddings = ces_model.encode(ces_questions['question_only'])
+ces_similarities = ces_model.similarity(ces_embeddings, ces_embeddings)
+ces_similarities.fill_diagonal_(float('-inf'))
 ces_max_values, ces_max_indices = torch.max(ces_similarities, dim=1)
 ces_dict = dict(zip(ces_questions['question_only'], ces_questions['question_only'].iloc[ces_max_indices] ))
 
+#list(ces_dict.items())
+
+test_string = 'What is the GDP of Afghanistan?'
+test_embedding = ces_model.encode(test_string)
+test_similarities = ces_model.similarity(test_embedding, ces_embeddings)
+test_max, test_index = torch.max(test_similarities, dim=1)
+ces_questions['question_only'].iloc[test_index]
+
+test_sort, test_indices = torch.sort(test_similarities, descending=True)
+test_matches = ces_questions['question_only'].iloc[test_indices[0]].values
+#reset_index()
+ces_questions[ces_questions['Topic'] == 'Foreign policy']
+np.where(test_indices[0] == 2424)
+ces_questions['question_only'].iloc[test_indices[0][range(680, 690)]]
